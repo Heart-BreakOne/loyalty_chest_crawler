360073d32712e732d66024e042cad50bdf1c616b
Leonardo Luiz <lcoutinholuiz@gmail.com>
2024-01-09 21:56:42 -0300

    Added extra instructions

--- .private_constants.py
+++ .private_constants.py
@@ -1,8 +1,12 @@
 
+# Rename this file by removing the dot.
+# The token can be accessed on Stream Raiders cookies: key ACCESS_INFO
+# The first slot needs to always be available, you may want to use an alternative account.
+
 # Your stream raiders credentials
 user_id = "" #ex "12345678v"
 token = "" #ex "12345678%xxxxxxxxxxxxx"
 
-# Your webhook url here
+# If you don't plan to post to discord webhook, ignore this section.
 discord = False
 webhook = "https://discord.com/api/webhooks/"
