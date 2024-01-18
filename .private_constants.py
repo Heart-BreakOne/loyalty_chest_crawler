2eea87c26b9b327b3a07c52ba202cda74da2437a
Leonardo Luiz <lcoutinholuiz@gmail.com>
2024-01-18 13:54:38 -0300

    Making user_id and access_info more clear to understand

--- .private_constants.py
+++ .private_constants.py
@@ -1,5 +1,5 @@
-# Fill this with your SR user ID and ACCESS_INFO. The user ID is at the beginning of the ACCESS_INFO.
+# Fill this with your SR user ID and ACCESS_INFO. The user ID is at the beginning of the ACCESS_INFO before the % sign.
 # The ACCESS_INFO can be found SR cookies with the key ACCESS_INFO
-user_id = "" #ex "12345678v"
+user_id = "" #ex "12345678"
 
 ACCESS_INFO = "" #ex "12345678%xxxxxxxxxxxxx"
